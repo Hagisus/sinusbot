a37b7344bd44c64d46e6a59d9d2f58f58bca52f9
GrzegorzKu <jem_obiadek@o2.pl>
2018-05-13 16:45:52 +0200

    unset() -> delete (PHP wont)

--- spy/html/assets/main.js
+++ spy/html/assets/main.js
@@ -91,7 +91,7 @@ $(function(){
 
           channels[channel.id] = $.extend({},channel)
         })
-        unset(_channels[0].next)
+        delete _channels[0].next
 
         //remove excess channels
         channels_tree[0] = _channels[0]
@@ -124,7 +124,7 @@ $(function(){
       changeInstance($t.data('uuid'), $t.text())
     })
   }
-  
+
   function renderChannel($root, channel){
     var element
     while(true){
