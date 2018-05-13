dbcf44f32d32e4a0514a9ac689b7e3ab34995a0a
GrzegorzKu <jem_obiadek@o2.pl>
2018-05-13 17:47:47 +0200

    Highligh bot

--- spy/html/assets/main.js
+++ spy/html/assets/main.js
@@ -137,7 +137,7 @@ $(function(){
   function renderClients($root, clients){
     clients.forEach( (client)=>{
       $("<li>")
-        .addClass("client")
+        .addClass("client" + (client.name == current_instance.name ? " me" : ""))
         .text(client.nick)
         .attr("id", "client_"+client.id)
         .data('client_id', client.id)
