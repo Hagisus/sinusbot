8bb84ce8ac28c872c10c37728c88a44f218cfd1e
GrzegorzKu <jem_obiadek@o2.pl>
2018-05-13 03:22:02 +0200

    Implement doRequest function

--- spy/html/assets/main.js
+++ spy/html/assets/main.js
@@ -4,10 +4,24 @@ $(function(){
 
   var address = "http://ts3.kgrzeg.pl:8087"
 
-  $.ajax(address+"/api/v1/bot/info",
-    {headers: {"Authorization": "bearer "+localStorage.getItem("token")}}
-  ).done(function(data){
-    console.log("Data received:", data)
-  })
+  function doRequest(url, data, done, fail, always){
+    var headers = {"Authorization": "bearer "+localStorage.getItem("token")}
+
+    if (data) headers["Content-Type"] = "application/json"
+
+    var method = data ? "POST" : "GET"
+    var send_data = data ? JSON.stringify(data) : undefined;
+
+    var request = $.ajax(address+url, {
+      headers: headers,
+      method: method,
+      dataType: 'json',
+      data: send_data,
+    } )
+
+    if (done)   request.done(done)
+    if (fail)   request.fail(fail)
+    if (always) request.always(always)
+  }
 
 })
