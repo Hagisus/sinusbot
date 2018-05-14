3acced20a11eec223f1d3399c539edc20c18422d
GrzegorzKu <jem_obiadek@o2.pl>
2018-05-14 14:25:36 +0200

    Add test echo event

--- spy.js
+++ spy.js
@@ -1,29 +1,38 @@
 registerPlugin({
-  name: 'Spy',
-  version: '0.1',
-  description: 'The bot will spy the entire server actions - users and channels',
-  author: 'Grzegorz Kupczyk <kupczykgrzeg@gmail.com>',
-  vars: [],
-  enableWeb: true
-  /*vars: [{
-      name: 'clientUids',
-      title: 'Comma-separated list of client-ids that the bot should follow',
-      type: 'string'
-  }]*/
+    name: 'Spy',
+    version: '0.1',
+    description: 'The bot will spy the entire server actions - users and channels',
+    author: 'Grzegorz Kupczyk <kupczykgrzeg@gmail.com>',
+    vars: [],
+    enableWeb: true
+    /*vars: [{
+        name: 'clientUids',
+        title: 'Comma-separated list of client-ids that the bot should follow',
+        type: 'string'
+    }]*/
 }, function (sinusbot, config) {
 
-  var engine = require('engine'),
-      backend = require('backend'),
-      event = require('event');
+    var engine = require('engine'),
+        backend = require('backend'),
+        event = require('event')
 
 
-  if (!config.clientUids) {
-      engine.log('Invalid clientUids');
-      return;
-  }
+    if (!config.clientUids) {
+        engine.log('Invalid clientUids')
+        return
+    }
 
-  channels = backend.getChannels();
-  clients = backend.getClients();
+    channels = backend.getChannels()
+    clients = backend.getClients()
 
-  engine.log('Spy initialized...');
-});
+    event.on("api:testecho", function (dateva) {
+        var res = "Returned: " + ev.data()
+
+        return {
+            success: true,
+            data: res
+        }
+    })
+
+    engine.log('Spy initialized...')
+})
